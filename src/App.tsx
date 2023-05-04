894bb3dcd2bcfc806858b709c9f2448e77ab2b95
Spiguerfild <luique16@outlook.com>
2023-05-03 22:57:08 -0400

    dia 03/05 app bar e tool bar botao e txtfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import { ChangeEvent, useState } from 'react'
-import { TextField } from '../node_modules/@mui/material/index'
+
+import { useTheme, TextField, Grid, AppBar, Toolbar, Typography, Container, Button } from '@mui/material';
 
 
 
 function App() {
-
+  const theme = useTheme()
   return (
     <>
-      <h1>To do list</h1>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <AppBar position='static'>
+        <Toolbar sx={{
+          paddingTop: theme.spacing(2),
+          paddingBottom: theme.spacing(2),
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          '@media all': {
+            minHeight: 200,
+          }
+        }}>
+          <Typography variant="h5" component="h6">
+            Scr
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container>
+          <Grid container spacing={theme.spacing(0.5)}>
+            <Grid item xl={10} xs={12}>
+              <TextField name='tasks' fullWidth />
+            </Grid>
+            <Grid item xl={2} xs={12} >
+              <Button variant='contained' name='tasks' fullWidth >wda</Button>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
     </>
   )
 }
