b84edcb884c8cfc44dd6a2d86ee7107650ee5390
Spiguerfild <luique16@outlook.com>
2023-05-05 00:34:25 -0400

    carga horaria ead

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 
-import { useTheme, TextField, Grid, AppBar, Toolbar, Typography, Container, Button, colors } from '@mui/material';
-import { PlusCircle, Rocket } from "@phosphor-icons/react";
+import { useTheme, TextField, Grid, AppBar, Toolbar, Typography, Container, Button, colors, Card, CardContent } from '@mui/material';
+import { ClipboardText, PlusCircle, Rocket } from "@phosphor-icons/react";
 import { styled } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+import { useState } from 'react';
 
 const CssTextField = styled(TextField)({
+  '& label': {
+    color: '#ffffff38'
+  },
   '& label.Mui-focused': {
     color: '#cfcfcf',
   },
 
   '& .MuiOutlinedInput-root': {
-
+    '& fieldset': {
+      borderColor: 'black',
+    },
     '&:hover fieldset': {
       borderColor: '#cfcfcf',
+      transition: '.4s',
     },
     '&.Mui-focused fieldset': {
       borderColor: '#cfcfcf',
@@ -21,7 +29,11 @@ const CssTextField = styled(TextField)({
 
 
 function App() {
+
+  const [cardLogica, setCardLogica] = useState(false)
+
   const theme = useTheme()
+
   return (
     <>
       <AppBar position='static'>
@@ -41,7 +53,7 @@ function App() {
             alignItems: 'center',
             gap: theme.spacing(2)
           }}>
-            <Rocket size={60} color='#eaec52' /><p style={{ color: '#5e60ce', fontWeight: '900', fontSize: '50px' }}> <span style={{ color: '#52b2ec' }}>to</span>do</p>
+            <Rocket size={60} color='#5e60ce' /><p style={{ color: '#5e60ce', fontWeight: '900', fontSize: '50px' }}> <span style={{ color: '#52b2ec' }}>to</span>do</p>
           </Typography>
         </Toolbar>
       </AppBar>
@@ -52,31 +64,56 @@ function App() {
             top: '-26px'
           }}>
             <Grid item xl={10} xs={12}>
-              <CssTextField name='tasks' variant='outlined' label='Task' placeholder='Adicione uma Nova tarefa' fullWidth sx={{ backgroundColor: colors.grey[900] }} />
+              <CssTextField name='tasks' variant='outlined' label='Task' placeholder='Adicione uma Nova tarefa' fullWidth sx={{
+                backgroundColor: colors.grey[900],
+
+              }} />
             </Grid>
             <Grid item xl={2} xs={12} >
               <Button variant='contained' name='tasks' fullWidth sx={{
                 height: '100%',
-                backgroundColor: '#cfcfcf',
+                backgroundColor: '#52b2ec',
                 '&:hover': {
-
-
+                  backgroundColor: '#2b7aab',
+                  transition: '.4s',
                 }
-              }}>Criar <PlusCircle size={30} /></Button>
+              }}>Create <PlusCircle size={30} /></Button>
             </Grid>
           </Grid>
         </Container>
 
 
       </main>
-      <Grid container spacing={theme.spacing(1)} sx={{ backgroundColor: 'white', paddingTop: '100px' }}>
-        <Grid item>
-          <Typography variant='caption'>
-            Tarefas Criadas
-          </Typography>
-        </Grid>
-      </Grid>
+      <Container sx={{ paddingTop: '100px' }}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '30px' }}>
+          <div><span style={{ color: '#52b2ec', fontSize: '20px', fontWeight: '600' }} >Tarefas criadas</span> 0</div>
+          <div><span style={{ color: '#5e60ce', fontSize: '20px', fontWeight: '600' }}>Concluidas</span> 0</div>
+        </div>
+        <Grid container spacing={theme.spacing(1)} >
+          <Grid item xl={12} xs={12}>
+            <Card sx={{
+              minWidth: 275,
+              width: '100%',
+              height: '55vh',
+              borderRadius: '10px',
 
+            }}>
+              <CardContent sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100%',
+                backgroundColor: '#121212'
+              }}>
+                <ClipboardText size={80} color={colors.grey[600]} />
+                <Typography variant='h6' color={colors.grey[600]}>Você ainda não tem tarefas cadastradas</Typography>
+                <Typography variant='h6' color={colors.grey[600]}>Crie tarefas e organize seus itens a fazer</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container >
     </>
   )
 }
