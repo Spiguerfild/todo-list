f15f60b04cf42c0e00d18f775cacb3674feebbf4
Spiguerfild <luique16@outlook.com>
2023-05-04 22:17:37 -0400

    colocando estilos e cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 
 import { useTheme, TextField, Grid, AppBar, Toolbar, Typography, Container, Button, colors } from '@mui/material';
 import { PlusCircle, Rocket } from "@phosphor-icons/react";
+import { styled } from '@mui/material/styles';
+
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#cfcfcf',
+  },
+
+  '& .MuiOutlinedInput-root': {
+
+    '&:hover fieldset': {
+      borderColor: '#cfcfcf',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#cfcfcf',
+    },
+  },
+});
 
 
 function App() {
@@ -24,36 +41,42 @@ function App() {
             alignItems: 'center',
             gap: theme.spacing(2)
           }}>
-            <Rocket size={60} color='#52b2ec' /><p style={{ color: '#5e60ce', fontWeight: '900', fontSize: '50px' }}> <span style={{ color: '#52b2ec' }}>to</span>do</p>
+            <Rocket size={60} color='#eaec52' /><p style={{ color: '#5e60ce', fontWeight: '900', fontSize: '50px' }}> <span style={{ color: '#52b2ec' }}>to</span>do</p>
           </Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <Container sx={{ position: 'relative' }}>
-          <Grid container spacing={theme.spacing(0.5)} sx={{
+        <Container sx={{ position: 'relative', }}>
+          <Grid container spacing={2} sx={{
             position: 'absolute',
             top: '-26px'
           }}>
             <Grid item xl={10} xs={12}>
-              <TextField name='tasks' fullWidth sx={{ backgroundColor: colors.grey[900] }} />
+              <CssTextField name='tasks' variant='outlined' label='Task' placeholder='Adicione uma Nova tarefa' fullWidth sx={{ backgroundColor: colors.grey[900] }} />
             </Grid>
             <Grid item xl={2} xs={12} >
               <Button variant='contained' name='tasks' fullWidth sx={{
-                height: '100%'
+                height: '100%',
+                backgroundColor: '#cfcfcf',
+                '&:hover': {
 
-              }}><PlusCircle size={32} /></Button>
-            </Grid>
 
-            <Grid container spacing={theme.spacing(1)}>
-              <Grid item>
-                <Typography variant='caption'>
-                  Tarefas Criadas
-                </Typography>
-              </Grid>
+                }
+              }}>Criar <PlusCircle size={30} /></Button>
             </Grid>
           </Grid>
         </Container>
+
+
       </main>
+      <Grid container spacing={theme.spacing(1)} sx={{ backgroundColor: 'white', paddingTop: '100px' }}>
+        <Grid item>
+          <Typography variant='caption'>
+            Tarefas Criadas
+          </Typography>
+        </Grid>
+      </Grid>
+
     </>
   )
 }
